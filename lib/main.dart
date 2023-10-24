3e24e08bdf68b61968109aa8590299752921a627
Muhammad Hasif <sayahas92@gmail.com>
2023-10-24 11:02:05 +0800

    Tambah libraries

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,7 +24,7 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  var nameEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -32,13 +34,13 @@ class HomePage extends StatelessWidget {
       body:SingleChildScrollView(
         child: Center(child: Column(
           children: [
-            Text("Hello World 2", style: TextStyle(fontSize: 32, color: Colors.blue)),
+            Text("Hello World 2", style: GoogleFonts.lato(textStyle: TextStyle(color: Colors.blue, fontSize:32))),
             SizedBox(height: 8,),
             Image.network("https://a.cdn-hotels.com/gdcs/production99/d1417/4da30a77-ecb2-4ead-a771-e61fac2a73dd.jpg"),
             SizedBox(height: 8,),
-            Text("This is my first app", style: TextStyle(fontSize: 28, color: Colors.green)),
+            Text("This is my first app", style: GoogleFonts.sofia(textStyle: TextStyle(fontSize: 28, color: Colors.green))),
             SizedBox(height: 8,),
-            Text("I hope you enjoy it!", style: TextStyle(fontSize: 18, color: Colors.purple),),
+            Text("I hope you enjoy it!", style: GoogleFonts.flamenco(textStyle: TextStyle(fontSize: 18, color: Colors.purple)),),
             SizedBox(height: 8,),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -48,11 +50,23 @@ class HomePage extends StatelessWidget {
                 Text("This is right")
               ],
             ),
+            TextField(decoration: InputDecoration(hintText: "Enter your name"),controller: nameEditingController,),
             TextButton(onPressed: (){
-              final snackBar1 = SnackBar(content: Text("Is this snackbar?"),
+              final snackBar1 = SnackBar(content: Text("Hello ${nameEditingController.text}"),
               action: SnackBarAction(label: "Yes", onPressed: (){print("Okay");},),);
               ScaffoldMessenger.of(context).showSnackBar(snackBar1);
-            }, child: Text("Press me!"))
+            }, child: Text("Press me!")),
+            ElevatedButton(onPressed: (){
+              Fluttertoast.showToast(
+                  msg: "Hello ${nameEditingController.text}",
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.CENTER,
+                  timeInSecForIosWeb: 1,
+                  backgroundColor: Colors.red,
+                  textColor: Colors.white,
+                  fontSize: 16.0
+              );
+            }, child: Text("Press toast"))
           ],
         )),
       )
